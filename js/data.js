c416932391bf91339b55256d2b79790de767dc44
Sergio Enrique Barrios <se.checo34@gmail.com>
2023-03-01 20:46:59 -0600

    Pantalla de Datos
    La pantalla donde el mocoso mete sus datos y lo doxxeamos

---
+++ js/data.js
@@ -0,0 +1,104 @@
+// Constantes del DOM
+const d = document;
+const formName = d.getElementById("nombre");
+const btn = d.getElementById("ingresar");
+const form = d.getElementById("player-form");
+const startGame = d.getElementById("start-game");
+
+// Variables del Local Storage
+let playerNames = localStorage.getItem("playerNames");
+playerNames = JSON.parse(playerNames);
+if (playerNames == null) playerNames = [];
+// Cuando cargue el documento
+d.addEventListener("DOMContentLoaded", () => {
+    iniciar();
+})
+
+function iniciar() {
+    // Todo el dibujado del canvas
+    const fondo = d.getElementById('fondo');
+    var canvas = d.getElementById("lienzo");
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(fondo, 90, 0, 1200, 805);
+}
+
+
+d.addEventListener("click", (e) => {
+    if (e.target.matches("#ingresar")) { // Verifica si presionamos el botón
+        e.preventDefault();
+        let name = formName.value;
+        verify(name);
+    }
+})
+
+function verify(name) {
+
+    let existentPlayer;
+    let match = false;
+    // Verficando si existe el nombre
+    for (let i = 0; i < playerNames.length; i++) {
+        let playerName = JSON.parse(playerNames[i]);
+        if (name == playerName.nombre) {
+            existentPlayer = playerName;
+            match = true;
+            break;
+        }
+    }
+
+    if (!match) {
+        let newPlayer = JSON.stringify({
+            nombre: name,
+            puntos: 0,
+            tiempo: 0
+        });
+
+        playerNames.push(newPlayer);
+        localStorage.setItem("playerNames", JSON.stringify(playerNames));
+        startScreen(existentPlayer, name);
+    } else {
+        startScreen(existentPlayer, name);
+    }
+
+}       
+
+// 
+function startScreen(playerName, name){
+    form.style.display = "none";
+    startGame.style.display = "inline-flex"
+    if(playerName == undefined){
+        let content = `
+            <p>Bienvenid@. Disfruta!</p>
+            <p>Nombre: ${name}</p>
+            <p>Mejor Puntaje: 0</p>
+            <p>Mejor Tiempo: 0</p>
+            
+        `;
+        startGame.innerHTML = content;
+        const play = d.createElement("button");
+        play.setAttribute("id", "playBtn");
+        play.innerHTML = `
+        <span class="material-symbols-outlined">
+            play_circle
+        </span>
+        
+        `
+        startGame.append(play);
+    }else{
+        let content = `
+            <p>Bienvenid@ de Vuelta</p>
+            <p>Nombre: ${playerName.nombre}</p>
+            <p>Mejor Puntaje: ${playerName.puntos}</p>
+            <p>Mejor Tiempo: ${playerName.tiempo}</p>
+        `
+        startGame.innerHTML = content;
+        const play = d.createElement("button");
+        play.setAttribute("id", "playBtn");
+        play.innerHTML = `
+        <span class="material-symbols-outlined">
+            play_circle
+        </span>
+        
+        `
+        startGame.append(play);
+    }
+}
