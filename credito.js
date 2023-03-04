613899817fdcf7ea96eb0bd9a39e0b893dbd14b6
ClaudiaSando <al248156@edu.uaa.mx>
2023-03-04 00:40:14 -0600

    creditos bien
    ghjklñ

--- credito.js
+++ credito.js
@@ -2,8 +2,12 @@
 function iniciar(){
     var canvas = document.getElementById("lienzo");
     var ctx = canvas.getContext("2d");
+<<<<<<< Updated upstream
     var canvas2 = document.getElementById("lienzo2");
     var ctx2 = canvas2.getContext("2d");
+=======
+
+>>>>>>> Stashed changes
     const fondo= document.getElementById('fondo');
     const logo = document.getElementById('logo');
 
@@ -18,13 +22,84 @@ function iniciar(){
         // Añadimos la fuente
         document.fonts.add(font);
         ctx.font = "40px Font name";
+<<<<<<< Updated upstream
         ctx.fillStyle = "white";
         ctx.textAlign = "center";
         ctx.fillText("BLUEY LITTLE BUG", canvas.width/4, 210);
+=======
+        ctx.shadowBlur= 0;
+        ctx.shadowColor = "";
+        ctx.fillStyle = "white";
+        ctx.textAlign = "center";
+        ctx.fillText("BLUEY LITTLE BUG", canvas.width/4, 210);
+
+
+        //universidad
+        ctx.font = "25px Font name";
+        ctx.fillStyle = "#FFDEAD";
+        ctx.textAlign = "center";
+        ctx.shadowBlur= 40;
+        ctx.shadowColor = "black";
+        ctx.fillText("Universidad Autonoma de Aguascalientes", canvas.width/1.5, 90);
+
+        //carrera
+        ctx.font = "25px Font name";
+        ctx.fillStyle = "#FFDEAD";
+        ctx.textAlign = "center";
+        ctx.fillText("Ingeniería en Sistemas", canvas.width/1.5, 130);
+
+        //semestre
+        ctx.font = "25px Font name";
+        ctx.fillStyle = "#FFDEAD";
+        ctx.textAlign = "center";
+        ctx.fillText("6° Semestre", canvas.width/1.5, 180);
+
+        //materia
+        ctx.font = "25px Font name";
+        ctx.fillStyle = "#FFDEAD";
+        ctx.textAlign = "center";
+        ctx.fillText("Tecnologías Web", canvas.width/1.5, 230);
+
+        //integrantes
+        ctx.font = "25px Font name";
+        ctx.fillStyle = "#FF8C00";
+        ctx.textAlign = "center";
+        ctx.fillText("Sergio Enrique Galvan Barrios", canvas.width/1.5, 280);
+
+        ctx.font = "25px Font name";
+        ctx.fillStyle = "#FF8C00";
+        ctx.textAlign = "center";
+        ctx.fillText("Alan Axel Escobar Calzada", canvas.width/1.5, 325);
+
+        ctx.font = "25px Font name";
+        ctx.fillStyle = "#FF8C00";
+        ctx.textAlign = "center";
+        ctx.fillText("Claudia Margarita Sandoval Chávez", canvas.width/1.5, 370);
+
+        ctx.font = "25px Font name";
+        ctx.fillStyle = "#FF8C00";
+        ctx.textAlign = "center";
+        ctx.fillText("Josue Emmanuel Uranga Calzada", canvas.width/1.5, 415);
+
+        
+
+        //fecha
+        ctx.font = "25px Font name";
+        ctx.fillStyle = "#FFDEAD";
+        ctx.textAlign = "center";
+        ctx.fillText("04 de Marzo de 2023", canvas.width/1.5, 465);
+
+        //ciudad
+        ctx.font = "25px Font name";
+        ctx.fillStyle = "#FFDEAD";
+        ctx.textAlign = "center";
+        ctx.fillText("Aguascalientes, Ags", canvas.width/1.3, 510);
+>>>>>>> Stashed changes
         
     })
     // Fuentes.
 
+<<<<<<< Updated upstream
     var segs = 0;
 
         function pad(val) {
@@ -60,6 +135,10 @@ function iniciar(){
 
 
 
+=======
+}
+
+>>>>>>> Stashed changes
 window.addEventListener('load', iniciar, false);
 
 
